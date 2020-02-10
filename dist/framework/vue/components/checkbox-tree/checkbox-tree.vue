aa0f0309d68c6f2b69c5eb1e19d38534d787b0b7
Tony Mongkolsmai <tony.mongkolsmai@intel.com>
2020-02-09 23:48:02 -0600

    Update to 2.2.1

--- dist/framework/vue/components/checkbox-tree/checkbox-tree.vue
+++ dist/framework/vue/components/checkbox-tree/checkbox-tree.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
-    <uwt-checkbox-tree-node
-      id="root"
-      :def="data"
-      :open="data.open"
-      :checked="data.checked"
-      :children="data.children"
-      :onCheckChanged="_onCheckChanged"
-      :onCheckCompleted="_onCheckCompleted"
-      class="checkbox-tree-node"
-    ></uwt-checkbox-tree-node>
-  </div>
+    <div>
+        <uwt-checkbox-tree-node
+            id="root"
+            :def="data"
+            :open="data.open"
+            :checked="data.checked"
+            :children="data.children"
+            :onCheckChanged="_onCheckChanged"
+            :onCheckCompleted="_onCheckCompleted"
+            class="checkbox-tree-node"
+        ></uwt-checkbox-tree-node>
+    </div>
 </template>
 
 <script>
@@ -18,37 +18,37 @@ import Vue from "vue";
 import UWTCheckboxTreeNode from "../checkbox-tree-node/checkbox-tree-node.vue";
 
 export default {
-  name: "uwtCheckboxTree",
-  components: {
-    UWTCheckboxTreeNode
-  },
-  props: {
-    data: {
-      type: Object,
-      default: () => {
-        return {};
-      }
+    name: "uwtCheckboxTree",
+    components: {
+        UWTCheckboxTreeNode
     },
-    onCheckChanged: {
-      type: Function,
-      default: undefined
+    props: {
+        data: {
+            type: Object,
+            default: () => {
+                return {};
+            }
+        },
+        onCheckChanged: {
+            type: Function,
+            default: undefined
+        },
+        onCheckCompleted: {
+            type: Function,
+            default: undefined
+        }
     },
-    onCheckCompleted: {
-      type: Function,
-      default: undefined
+    methods: {
+        _onCheckChanged() {
+            if (this.onCheckChanged) {
+                this.onCheckChanged();
+            }
+        },
+        _onCheckCompleted() {
+            if (this.onCheckCompleted) {
+                this.onCheckCompleted();
+            }
+        }
     }
-  },
-  methods: {
-    _onCheckChanged() {
-      if (this.onCheckChanged) {
-        this.onCheckChanged();
-      }
-    },
-    _onCheckCompleted() {
-      if (this.onCheckCompleted) {
-        this.onCheckCompleted();
-      }
-    }
-  }
 };
 </script>
